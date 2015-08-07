9bc736a3f1e8e5ef9f1dbe448dc597fc3db8e8ff
laquino <akku@outlook.com>
2015-08-07 08:07:27 +0200

    routing

--- projects/kafa/js/app.js
+++ projects/kafa/js/app.js
@@ -1,16 +1,14 @@
 angular.module('kafa', ['ngRoute'])
 .config(function($routeProvider, $locationProvider) {
   $routeProvider
-//  .when('/info', {
-//    templateUrl: 'pages/info.html'
-//  })
   .when('/:nav', {
     templateUrl: 'router.html',
 	controller: 'RouteController'
   })
   .otherwise({
 	redirectTo: '/info'
-  })
+  });
+  $locationProvider.html5Mode(true);
 })
 .controller('RouteController', function ($scope, $routeParams) {
 	$scope.templateUrl = 'pages/' + $routeParams.nav + '.html';
